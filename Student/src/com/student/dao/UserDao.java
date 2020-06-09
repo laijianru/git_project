52e9725ed94bcdf305919e8b4e58e5d6c5c4c3d8
laijianru <2307782014@qq.com>
2020-06-09 15:24:28 +0800

    提交到本次仓库

---
+++ Student/src/com/student/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.student.dao;
+
+public class UserDao {
+
+	public void addUser(int id,String username,String password) {
+		
+	}
+	
+	public void deleteUser(int id) {
+		
+	}
+	
+	public void updataUser(int id,String newusername,String newpassword) {
+		
+	}
+}
